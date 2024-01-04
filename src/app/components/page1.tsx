6d40773b41446e4f0255cb8f055f8c51b30cad3a
João Victor <jvbatistagomes11@gmail.com>
2024-01-04 14:55:32 -0300

    Final commit

--- src/app/components/page1.tsx
+++ src/app/components/page1.tsx
@@ -14,7 +14,7 @@ export default function InicialPage() {
             </div>
             <div className='text-white p-10 mb-12 grid md:order-first md:gap-5'>
                 <h2 className='font-bold uppercase text-xl my-3 md:text-2xl'>Welcome</h2>
-                <h1 className='text-2xl font-semibold my-3 md:text-7xl'>Silem Mulambé Kaidros</h1>
+                <h1 className='text-2xl font-semibold my-3 md:text-7xl'>Lorem ipsum dolor sit amet consectetur </h1>
                 <span className='text-lg font-light md:text-2xl'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit nemo hic quos, ab, dolor aperiam nobis cum est eos error ipsum, voluptate culpa nesciunt delectus iste?</span>
                 <button className=' bg-white w-48 hover:bg-zinc-300 shadow-lg text-black text-xl px-12 py-3 my-10 rounded-md btnMd'>
                     Explore
